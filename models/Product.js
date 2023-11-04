6e2853b181c2c9758453729b83674dd00dcd389e
Hello-AndyJoe <andrew.joseph.roper@gmail.com>
2023-11-04 14:55:09 -0700

    Minor edits to models but I cannot get the associations to work.

--- models/Product.js
+++ models/Product.js
@@ -21,12 +21,19 @@ Product.init(
       allowNull: false,
     },
     price: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.DECIMAL(10.2),
       allowNull: false,
+      validate: {
+        isDecimal: true
+      }
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isInt: true
+      }
     },
     category_id: {
       type: DataTypes.INTEGER,
