2b6ea4b468a5cd2a2547e3c1e9f862ec54689642
Hello-AndyJoe <andrew.joseph.roper@gmail.com>
2023-11-04 17:16:46 -0700

    Built out the POST, PUT, DELETE, GET routers that do not need associations.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,25 +6,52 @@ const { Category, Product } = require('../../models');
 router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
-  const categorytData = await Category.findAll();
-  return res.json(categorytData);
+  const categoryData = await Category.findAll();
+  return res.json(categoryData);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  const categoryData = await Category.findAll (
+    {
+      where: {
+        id: req.params.id
+      },
+      // include: [
+      //   {model: Product},
+      // ]
+    }
+  );
+  return res.json(categoryData);
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+
+  const categoryData = await Category.create(req.body);
+  return res.json(categoryData);
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+
+  const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    return res.json(categoryData);
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  const categoryData = await Category.destroy ({
+    where: {
+      id: req.params.id,
+    }
+  })
+  return res.json(categoryData);
 });
 
 module.exports = router;
